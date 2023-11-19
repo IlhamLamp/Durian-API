e9dd7554e18f5f518c5889d79d8ef0078d1006a6
IlhamLamp <ilhamutomo24@gmail.com>
2023-11-19 10:07:51 +0700

    add: del role

--- src/controllers/RoleController.ts
+++ src/controllers/RoleController.ts
@@ -123,7 +123,7 @@ const UpdateRole = async (req: Request, res: Response): Promise<Response> => {
 
         return res.status(200).send({
             status: 200,
-            message: 'Success Update Selected Role',
+            message: 'Successfuly Update Selected Role',
             data: role
         });
 
@@ -144,4 +144,43 @@ const UpdateRole = async (req: Request, res: Response): Promise<Response> => {
     }
 }
 
-export default {GetRole, GetRoleById, CreateRole, UpdateRole};
+const DeleteRole = async (req: Request, res: Response): Promise<Response> => {
+    try {
+
+        const {id} = req.params;
+        const role = await Role.findByPk(id);
+
+        if (!role) {
+            return res.status(404).send({
+                status: 404,
+                message: 'Role Not Found',
+                data: null
+            });
+        }
+
+        await role.destroy();
+
+        return res.status(200).send({
+            status: 200,
+            message: 'Successfuly Delete Selected Role',
+            data: null
+        });
+
+    } catch (error: any) {
+        if (error != null && error instanceof Error) {
+            return res.status(500).send({
+                status: 500,
+                message: error.message,
+                errors: error
+            });
+        }
+
+        return res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error',
+            errors: error
+        });
+    }
+}
+
+export default {GetRole, GetRoleById, CreateRole, UpdateRole, DeleteRole};
--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get("/role", RoleController.GetRole);
 router.post("/role", RoleController.CreateRole);
 router.get("/role/:id", RoleController.GetRoleById);
-router.post("/role/:id", RoleController.UpdateRole);
+router.patch("/role/:id", RoleController.UpdateRole);
+router.delete("/role/:id", RoleController.DeleteRole);
 
 export default router;
